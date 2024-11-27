f572e0a2a881bf2bc1157da4fed57e195bcfde0b
sofffja <sofi.efron@gmail.com>
2024-11-27 23:27:34 +0000

    Create tailwind color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,19 @@
+import { g } from "framer-motion/client";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        light: "#fff",
+        dark: "#000",
+        gray: "#aaa",
+        darkGray: "#666",
+        primary: "#00565f",
+        secondary: "#e75d13",
+      },
+    },
   },
   plugins: [],
   darkMode: "selector",
