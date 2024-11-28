2aee314d6873d76b5ae9e5dc502f94d91e64896d
sofffja <sofi.efron@gmail.com>
2024-11-28 02:21:33 +0000

    Update vite config to address chunks issues

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/terraignota-map/',
-})
+  base: "/terraignota-map/",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          maplibre: ["maplibre-gl"], // Separate MapLibre into its own chunk
+        },
+      },
+    },
+    assetsDir: "assets",
+    chunkFileNames: "assets/[name]-[hash].js", // Ensure chunks have hashes
+  },
+});
