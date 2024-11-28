fcf5976116c9bec850db15ba12eeb2618214a768
sofffja <sofi.efron@gmail.com>
2024-11-28 19:33:51 +0000

    Add NavLink and center map on click to item markers

--- src/components/Map/ItemMarker.jsx
+++ src/components/Map/ItemMarker.jsx
@@ -1,6 +1,7 @@
 import { Marker, useMap } from "@vis.gl/react-maplibre";
+import { NavLink } from "react-router-dom";
 
-export default function ItemMarker({ item }) {
+export default function ItemMarker({ item, setCurrentItem }) {
   const { current: map } = useMap();
 
   if (!map) return null;
@@ -12,7 +13,18 @@ export default function ItemMarker({ item }) {
       anchor="bottom"
       mapStyle="/terraignota-map/styles/light.json"
     >
-      <div className="size-4 cursor-pointer border border-[#000] bg-[#0f0] p-[2px] text-xs text-[#00464F]" />
+      <NavLink to={`/${item.slug.current}`}>
+        <div
+          onClick={() => {
+            setCurrentItem(item);
+            map.flyTo({
+              center: [item.long, item.lat],
+              zoom: 8,
+            });
+          }}
+          className="size-4 cursor-pointer border border-[#000] bg-[#0f0] p-[2px] text-xs text-[#00464F]"
+        />
+      </NavLink>
     </Marker>
   );
 }
