a85ef65f8e4e41cbb861f1cf43be546e843c2137
sofffja <sofi.efron@gmail.com>
2024-12-26 18:34:53 +0000

    Refactor Nav component layout to improve responsiveness

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,7 +15,7 @@ export default function Nav({
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="pointer-events-none fixed inset-0 flex h-full w-1/2 shrink-0 flex-col justify-between px-6 pb-6 pt-4">
+    <div className="pointer-events-none fixed inset-0 flex h-full w-1/2 flex-col justify-between px-6 pb-6 pt-4">
       <div
         onClick={() => setIsNavOpen(!isNavOpen)}
         className="pointer-events-auto size-6 shrink-0 cursor-pointer rounded-full border border-gray bg-light pt-1 text-center text-xs shadow-md dark:bg-dark"
@@ -26,7 +26,7 @@ export default function Nav({
       {isNavOpen && (
         <>
           <div className="ml-3 h-full w-[1px] border border-gray" />
-          <div className="flex h-2/3 flex-col gap-2">
+          <div className="flex h-2/3 shrink-0 flex-col gap-2">
             <ProjectList
               projects={data?.projects}
               selectedProjects={selectedProjects}
