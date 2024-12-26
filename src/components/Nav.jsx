609b24f420fc945488d6fe0d4c7fccdf05f30fc1
astrosuka <yegresvoselok@gmail.com>
2024-12-26 18:36:12 -0300

    Refactor Nav component button

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -15,14 +15,11 @@ export default function Nav({
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="pointer-events-none fixed inset-0 flex h-full w-1/2 flex-col justify-between px-6 pb-6 pt-4">
+    <div className="pointer-events-none fixed inset-0 flex h-full w-1/2 flex-col justify-end px-6 pb-6 pt-4 transition-all">
       <div
         onClick={() => setIsNavOpen(!isNavOpen)}
-        className="pointer-events-auto size-6 shrink-0 cursor-pointer rounded-full border border-gray bg-light pt-1 text-center text-xs shadow-md dark:bg-dark"
-      >
-        {isNavOpen ? "X" : "-"}
-      </div>
-
+        className={`pointer-events-auto flex size-8 shrink-0 cursor-pointer items-center justify-center ${isNavOpen ? "bg-light dark:bg-dark" : "bg-dark dark:bg-light"} border border-gray p-2 text-center text-xs shadow-md hover:invert`}
+      />
       {isNavOpen && (
         <>
           <div className="ml-3 h-full w-[1px] border border-gray" />
