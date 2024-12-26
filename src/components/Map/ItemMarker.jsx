c2128743f9486f9a2a0238177faf653039eafa63
sofffja <sofi.efron@gmail.com>
2024-12-26 21:39:01 +0000

    Enhance ItemMarker component with opacity transitions

--- src/components/Map/ItemMarker.jsx
+++ src/components/Map/ItemMarker.jsx
@@ -25,13 +25,13 @@ export default function ItemMarker({ item, setCurrentItem, currentItem }) {
           style={{
             background: `radial-gradient(circle, ${chooseColor(item.project._id)} 10%, #ffffff00 70%)`,
           }}
-          className={`absolute size-8 cursor-pointer rounded-full text-xs ${item == currentItem || currentItem == null ? "" : "opacity-40"}`}
+          className={`absolute size-8 cursor-pointer rounded-full text-xs transition-opacity duration-500 ${item == currentItem || currentItem == null ? "opacity-100" : "opacity-40"}`}
         />
         <div
-          className={`absolute size-4 rounded-full border border-dark ${item == currentItem || currentItem == null ? "" : "opacity-40"}`}
+          className={`absolute size-4 rounded-full border border-dark transition-opacity duration-500 ${item == currentItem || currentItem == null ? "opacity-100" : "opacity-40"}`}
         />
         <div
-          className={`absolute size-12 animate-spin rounded-full dark:invert ${item == currentItem ? "" : "opacity-0"}`}
+          className={`absolute size-12 animate-spin rounded-full transition-opacity duration-500 dark:invert ${item == currentItem ? "opacity-100" : "opacity-0"}`}
           style={{
             backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='black' stroke-width='1' stroke-dasharray='16%2c 2%2c 16' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e")`,
           }}
