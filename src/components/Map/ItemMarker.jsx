dd015ace1315fbb88ba2dbe32aad6b4c59913b63
sofffja <sofi.efron@gmail.com>
2024-12-26 20:50:54 +0000

    Enhance ItemMarker component with animated border and custom background

--- src/components/Map/ItemMarker.jsx
+++ src/components/Map/ItemMarker.jsx
@@ -31,7 +31,10 @@ export default function ItemMarker({ item, setCurrentItem, currentItem }) {
           className={`absolute size-4 rounded-full border border-dark ${item == currentItem || currentItem == null ? "" : "opacity-40"}`}
         />
         <div
-          className={`absolute size-12 rounded-full border border-dashed border-dark ${item == currentItem ? "" : "opacity-0"}`}
+          className={`absolute size-12 animate-spin rounded-full dark:invert ${item == currentItem ? "" : "opacity-0"}`}
+          style={{
+            backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='black' stroke-width='1' stroke-dasharray='16%2c 2%2c 16' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e")`,
+          }}
         />
       </NavLink>
     </Marker>
