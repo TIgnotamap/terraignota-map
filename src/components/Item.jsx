662d7861b77d90526f708803daba3b912673c16d
sofffja <sofi.efron@gmail.com>
2025-01-03 21:51:23 +0000

    Update Item component to conditionally render VideoContainer based on video presence

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -9,13 +9,13 @@ export default function Item({ currentItem }) {
         <ImageSlideshow images={currentItem.images} />
       )}
 
-      {currentItem?.template === "2" && (
+      {currentItem?.template === "2" && currentItem?.video && (
         <div className="fixed left-[50%] top-36 w-2/6 -translate-x-1/2 px-6 drop-shadow-lg">
           <VideoContainer item={currentItem} />
         </div>
       )}
 
-      {currentItem?.template === "3" && (
+      {currentItem?.template === "3" && currentItem?.video && (
         <div className="fixed right-0 top-36 w-[calc(100%-224px)] px-6">
           <VideoContainer item={currentItem} />
         </div>
