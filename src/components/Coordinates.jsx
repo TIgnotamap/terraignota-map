33e6dd71841476b5ce9be3d05fec555137f7e983
astrosuka <yegresvoselok@gmail.com>
2025-01-03 21:36:19 -0300

    Update Coordinates for consistent styling

--- src/components/Coordinates.jsx
+++ src/components/Coordinates.jsx
@@ -1,6 +1,6 @@
 export default function Coordinates({ currentItem, mapCenter }) {
   return (
-    <div className="flex gap-8">
+    <div className="flex gap-8 font-serif">
       <div className="bg-[radial-gradient(#fff_0%,_#ffffff00_80%)] drop-shadow-[0_0_2px_#fff] dark:bg-[radial-gradient(#000_0%,_#00000000_80%)] dark:drop-shadow-[0_0_2px_#000]">
         {currentItem?.lat || mapCenter.lat}
       </div>
