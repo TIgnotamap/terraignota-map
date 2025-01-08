ee27335bf56a1e44db20c94cb344532aa6b08337
sofffja <sofi.efron@gmail.com>
2025-01-08 23:12:23 +0000

    Enhance StatusBar styling with colors, rounded corners and more padding

--- src/components/ui/StatusBar.jsx
+++ src/components/ui/StatusBar.jsx
@@ -10,7 +10,7 @@ export default function StatusBar({ data }) {
   }, [data]);
 
   return (
-    <div className="bo pointer-events-none fixed left-[5rem] top-4 max-w-[33vw] border border-gray bg-light px-1 font-serif text-sm dark:bg-dark">
+    <div className="bo pointer-events-none fixed left-[5rem] top-4 max-w-[33vw] rounded-md border border-lightGray bg-light px-2 py-0.5 font-mono text-xs text-darkGray dark:border-darkGray dark:bg-dark dark:text-gray">
       {status || defaultStatus}
     </div>
   );
