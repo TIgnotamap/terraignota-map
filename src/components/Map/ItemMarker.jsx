0cf73634f678080ae513b13abe6c67b3119edd18
sofffja <sofi.efron@gmail.com>
2025-01-08 21:28:44 +0000

    Update ItemMarker component to hide selected item animation for better hover states

--- src/components/Map/ItemMarker.jsx
+++ src/components/Map/ItemMarker.jsx
@@ -50,7 +50,7 @@ export default function ItemMarker({
           className={`absolute size-4 rounded-full border border-dark transition-opacity duration-500 ${item == currentItem || item == hoveredItem || (hoveredItem == null && currentItem == null) ? "z-[100] opacity-100" : "opacity-20"}`}
         />
         <div
-          className={`absolute size-12 animate-spin rounded-full transition-opacity duration-500 dark:invert ${item == currentItem ? "opacity-100" : "opacity-0"}`}
+          className={`absolute size-12 animate-spin rounded-full transition-opacity duration-500 dark:invert ${item == currentItem ? "opacity-100" : "hidden opacity-0"}`}
           style={{
             backgroundImage: `url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='100' ry='100' stroke='black' stroke-width='1' stroke-dasharray='16%2c 2%2c 16' stroke-dashoffset='0' stroke-linecap='round'/%3e%3c/svg%3e")`,
           }}
