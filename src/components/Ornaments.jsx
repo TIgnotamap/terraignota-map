c60cbbc6c4a6645159523649cdc4ef83d687fa23
sofffja <sofi.efron@gmail.com>
2025-01-10 18:12:33 +0000

    Change Ornaments lines length

--- src/components/Ornaments.jsx
+++ src/components/Ornaments.jsx
@@ -4,9 +4,9 @@ export default function Ornaments({ currentItem }) {
       aria-hidden="true"
       className="pointer-events-none fixed inset-0 -z-10 flex h-screen w-full flex-col items-center justify-between"
     >
-      <div className="h-8 w-[1px] bg-dark dark:bg-light" />
+      <div className="h-6 w-[1px] bg-dark dark:bg-light" />
       <div className="flex w-full items-center justify-between">
-        <div className="h-[1px] w-8 bg-dark dark:bg-light" />
+        <div className="h-[1px] w-6 bg-dark dark:bg-light" />
         <div
           className={`text-2xl transition-opacity dark:invert ${currentItem ? "opacity-0" : "opacity-100"}`}
         >
@@ -20,9 +20,9 @@ export default function Ornaments({ currentItem }) {
             <path d="M480-420q-25.31 0-42.65-17.35Q420-454.69 420-480t17.35-42.65Q454.69-540 480-540t42.65 17.35Q540-505.31 540-480t-17.35 42.65Q505.31-420 480-420Zm-20-220v-160h40v160h-40Zm0 480v-160h40v160h-40Zm180-300v-40h160v40H640Zm-480 0v-40h160v40H160Z" />
           </svg>
         </div>
-        <div className="h-[1px] w-8 bg-dark dark:bg-light" />
+        <div className="h-[1px] w-6 bg-dark dark:bg-light" />
       </div>
-      <div className="h-8 w-[1px] bg-dark dark:bg-light" />
+      <div className="h-6 w-[1px] bg-dark dark:bg-light" />
     </div>
   );
 }
