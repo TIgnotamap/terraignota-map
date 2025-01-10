0956cd4833ac65e4bede50008305af07ef4651f2
sofffja <sofi.efron@gmail.com>
2025-01-10 17:59:40 +0000

    Refactor Coordinates component for consistent length

--- src/components/Coordinates.jsx
+++ src/components/Coordinates.jsx
@@ -1,11 +1,11 @@
 export default function Coordinates({ currentItem, mapCenter }) {
   return (
-    <div className="flex gap-8 font-serif">
-      <div className="bg-[radial-gradient(#fff_0%,_#ffffff00_80%)] drop-shadow-[0_0_2px_#fff] dark:bg-[radial-gradient(#000_0%,_#00000000_80%)] dark:drop-shadow-[0_0_2px_#000]">
-        {currentItem?.lat || mapCenter.lat}
+    <div className="flex gap-12 font-serif sm:gap-8">
+      <div className="drop-shadow-[0_0_2px_#fff] sm:bg-[radial-gradient(#fff_0%,_#ffffff00_80%)] dark:drop-shadow-[0_0_2px_#000] dark:sm:bg-[radial-gradient(#000_0%,_#00000000_80%)]">
+        {currentItem?.lat || mapCenter.lat.toFixed(6)}
       </div>
-      <div className="bg-[radial-gradient(#fff_0%,_#ffffff00_80%)] drop-shadow-[0_0_2px_#fff] dark:bg-[radial-gradient(#000_0%,_#00000000_80%)] dark:drop-shadow-[0_0_2px_#000]">
-        {currentItem?.long || mapCenter.lng}
+      <div className="drop-shadow-[0_0_2px_#fff] sm:bg-[radial-gradient(#fff_0%,_#ffffff00_80%)] dark:drop-shadow-[0_0_2px_#000] dark:sm:bg-[radial-gradient(#000_0%,_#00000000_80%)]">
+        {currentItem?.long || mapCenter.lng.toFixed(6)}
       </div>
     </div>
   );
