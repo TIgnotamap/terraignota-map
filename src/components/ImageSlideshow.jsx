c127182369f19b828478906f0fe61107c75ff841
sofffja <sofi.efron@gmail.com>
2025-01-12 01:23:54 +0000

    Add conditional rendering for resizeImage button based on image load status and update styling

--- src/components/ImageSlideshow.jsx
+++ src/components/ImageSlideshow.jsx
@@ -63,20 +63,23 @@ export default function ImageSlideshow({ images }) {
           </div>
         ))}
 
-        <div
-          className="absolute right-3 top-3 hidden size-4 cursor-pointer items-center justify-center rounded-sm border bg-white hover:size-5 sm:flex"
-          onClick={resizeImage}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            height="0.75rem"
-            viewBox="0 -960 960 960"
-            width="0.75rem"
-            fill="undefined"
+        {loadedImages[images[0]?._key] && (
+          <div
+            className="absolute right-3 top-3 hidden size-6 cursor-pointer items-center justify-center rounded-full border border-gray bg-light hover:invert sm:flex dark:bg-dark"
+            onClick={resizeImage}
           >
-            <path d="M160-160v-176.92h40V-200h136.92v40H160Zm463.85 0v-40h136.92v-136.92h40V-160H623.85ZM160-623.08V-800h176.92v40H200v136.92h-40Zm600.77 0V-760H623.85v-40h176.92v176.92h-40Z" />
-          </svg>
-        </div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              height="0.9rem"
+              viewBox="0 -960 960 960"
+              width="0.9rem"
+              fill="#000"
+              className="opacity-60 dark:invert"
+            >
+              <path d="M100.78-100.78V-334.7h106v127.92H334.7v106H100.78Zm525.09 0v-106h127.35V-334.7h106v233.92H625.87ZM100.78-625.87v-233.35H334.7v106H206.78v127.35h-106Zm652.44 0v-127.35H625.87v-106h233.35v233.35h-106Z" />
+            </svg>
+          </div>
+        )}
       </div>
 
       <AnimatePresence>
@@ -101,17 +104,18 @@ export default function ImageSlideshow({ images }) {
             ))}
 
             <div
-              className="absolute right-3 top-3 flex size-4 cursor-pointer items-center justify-center rounded-sm border bg-white hover:size-5"
+              className="absolute right-3 top-3 hidden size-6 cursor-pointer items-center justify-center rounded-full border border-gray bg-dark hover:invert sm:flex dark:bg-light"
               onClick={resizeImage}
             >
               <svg
                 xmlns="http://www.w3.org/2000/svg"
-                height="0.75rem"
+                height="0.9rem"
                 viewBox="0 -960 960 960"
-                width="0.75rem"
-                fill="undefined"
+                width="0.9rem"
+                fill="#fff"
+                className="opacity-60 dark:invert"
               >
-                <path d="M296.92-160v-136.92H160v-40h176.92V-160h-40Zm326.93 0v-176.92h176.92v40H663.85V-160h-40ZM160-623.08v-40h136.92V-800h40v176.92H160Zm463.85 0V-800h40v136.92h136.92v40H623.85Z" />
+                <path d="M228.7-100.78V-228.7H100.78v-106H334.7v233.92h-106Zm397.17 0V-334.7h233.35v106H731.87v127.92h-106ZM100.78-625.87v-106H228.7v-127.35h106v233.35H100.78Zm525.09 0v-233.35h106v127.35h127.35v106H625.87Z" />
               </svg>
             </div>
           </motion.div>
